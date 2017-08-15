381385565ecdec589cc86c0d7a9ee610356edd1f
Karel <kguerra@fvi.edu>
2017-08-14 21:45:54 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,33 @@
+const api_url = 'https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch='
+const wiki_link = 'https://en.wikipedia.org/wiki'
+
+const articleContainer = document.querySelector('.articles-container')
+
+function getArticles(searchTerm) {
+  $.ajax({
+    url: api_url + searchTerm,
+    dataType: 'jsonp'
+  }).done(function(response) {
+    console.log(response);
+    
+
+    for (let i = 0; i < response.query.search.length; i++) {
+      const title = response.query.search[i].title
+      const snippet = response.query.search[i].snippet
+
+      articleContainer.innerHTML += `
+  <a class="article" href="${wiki_link}/${title}" target="_blank" rel="noopener">
+  <h3 class="article-title">${title}</h3>
+  <p class="article-snippet">${snippet}</p>
+  </a>
+  `
+    }
+  })
+}
+
+
+searchForm.addEventListener('submit', function(event) {
+  event.preventDefault()
+articleContainer.innerHTML = ''
+  getArticles(search.value)
+})
